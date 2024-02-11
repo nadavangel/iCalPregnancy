048a3b1c4bbeb2106ef54d23670017e4be16aed8
Nadav Angel <nangel@parallelwireless.com>
2024-02-11 15:27:20 +0200

    Use waitress

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 
 import time
 import os
-
+from waitress import serve
 from calenderCalc.iCal import PregnancyICal
 
 app = Flask('PregnancyCalendar')
@@ -73,4 +73,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    serve(app=app, host='0.0.0.0', port=5000)
